dec0a8a6a23540d82c457bc677f6de975c9468ad
Erkin <erkinred@gmail.com>
2022-07-16 00:48:37 +0600

    code_2

---
+++ code_1.js
@@ -0,0 +1,13 @@
+'ude strict';
+
+const storeName = "Erkin";
+
+const storeDescription = {
+    budget: 10000,
+    employees: ['Azamat', 'Dauren', 'Ramazan'],
+    products: {
+        cola: 200,
+        pepsi: 220
+    },
+    open: true
+}
